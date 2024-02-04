e0dbce999caf9921eda8e03464bf9a1c8260801a
canonicalized <office@canonicalized.com>
2024-02-04 17:19:55 +0200

    Update ingest_geojson.sql

--- ingest_geojson.sql
+++ ingest_geojson.sql
@@ -1,6 +1,78 @@
 USE DATABASE AQ_CHICAGO;
 USE SCHEMA RAW;
 
+-- create or replace file format JSON_FORMAT
+-- type = json;
+
+
+-- create OR REPLACE stage GEOJSON
+-- FILE_FORMAT = JSON_FORMAT;
+
+ls @geojson;
+
+-- read file contents
+select 
+    METADATA$FILENAME::STRING as FILE_NAME
+  , METADATA$FILE_ROW_NUMBER as ROW_NUMBER
+  , $1:features::VARIANT as CONTENTS
+from @GEOJSON
+  (file_format => 'JSON_FORMAT')
+order by 
+    FILE_NAME
+  , ROW_NUMBER
+;
+
+-- Create a table in which to land the data
+create or replace table RAW_DATA (
+    FILE_NAME STRING
+  , ROW_NUMBER INT
+  , RAW_JSON VARIANT
+)
+;
+
+-- Ingest the data
+COPY INTO RAW_DATA
+FROM (
+  select 
+    METADATA$FILENAME::STRING as FILE_NAME
+  , METADATA$FILE_ROW_NUMBER as ROW_NUMBER
+  , $1:features::VARIANT as CONTENTS
+  from @GEOJSON
+    (file_format => 'JSON_FORMAT')
+);
+
+-- --using staged data
+-- SELECT  r.index, r.value
+-- FROM @GEOJSON
+-- ,TABLE(FLATTEN($1:features)) r;
+
+
+-- create table with flattened data
+CREATE OR REPLACE TABLE FLATTENED_DATA AS
+    SELECT  r.index AS INDEX
+        ,r.value:properties:"TITLE"::string as TITLE
+        ,r.value:properties:"FromBreak"::int as FROM_BREAK
+        ,r.value:properties:"ToBreak"::int as TO_BREAK
+        ,r.value:properties:"AnalysisAr"::float as ANALYSIS_AREA
+        , TO_GEOGRAPHY(r.value:geometry) AS geom
+        , ST_ASWKT(TO_GEOGRAPHY(r.value:geometry)) AS wkt
+    FROM RAW_DATA
+    ,TABLE(FLATTEN(RAW_DATA.RAW_JSON)) r;
+
+SELECT * FROM FLATTENED_DATA;
+
+
+
+
+
+
+
+
+
+--chicago boundaries from geojson file - single row
+USE DATABASE AQ_CHICAGO;
+USE SCHEMA RAW;
+
 create or replace file format JSON_FORMAT
 type = json;
 
